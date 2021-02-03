bea5c183761f0e48ef43a3fc6b3486446b722a97
bhagyash14 <72669538+bhagyash14@users.noreply.github.com>
2021-02-03 11:14:02 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ from models import setup_db, Movies, Actors
 from auth import AuthError, requires_auth
 
 
-class TriviaTestCase(unittest.TestCase):
+class CapstoneTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
     def setUp(self):
@@ -18,6 +18,20 @@ class TriviaTestCase(unittest.TestCase):
         self.database_name = "capstone"
         self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
+
+
+        self.assistant_header = {
+            "Content-Type": "application/json",
+            "Authorization":  os.environ.get('ASSISTANT_TOKEN')
+        }
+        self.director_header = {
+            "Content-Type": "application/json",
+            "Authorization":  os.environ.get('DIRECTOR_TOKEN')
+        }
+        self.producer_header = {
+            "Content-Type": "application/json",
+            "Authorization":  os.environ.get('PRODUCER_TOKEN')
+        }
 
         # binds the app to the current context
         with self.app.app_context():
@@ -41,91 +55,101 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
 
     def testGetActorsSuccess(self):
-        res = self.client().get('/actors')
+        res = self.client().get('/actor',headers={"Authorization": "Bearer {}".
+                                format(self.assistant_header)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['message'])
 
     def testGetActorsFailure(self):
         res = self.client().get('/actorrrs')
         self.assertEqual(res.status_code, 404)
 
     def testGetMoviesSuccess(self):
-        res = self.client().get('/movies')
+        res = self.client().get('/movie',headers={"Authorization": "Bearer {}".
+                                format(self.assistant_header)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['message'])
 
     def testGetMoviesFailure(self):
         res = self.client().get('/moviess')
         self.assertEqual(res.status_code, 404)
 
     def testPostActorSuccess(self):
-        res = self.client().post('/actors', json=self.new_actor)
+        res = self.client().post('/actors', json=self.new_actor,
+                                 headers={"Authorization": "Bearer {}".
+                                          format(self.director_header)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['message'])
+        self.assertTrue(data['message'],'Successfully added actor')
 
     def testPostActorFailure(self):
         res = self.client().post('/actorsss', json=self.new_actor)
         self.assertEqual(res.status_code, 404)
 
     def testPostMovieSuccess(self):
-        res = self.client().post('/movies', json=self.new_movie)
+        res = self.client().post('/movies', json=self.new_movie,
+                                 headers={"Authorization": "Bearer {}".
+                                          format(self.producer_header)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['message'])
+        self.assertTrue(data['message'],'Successfully added movie')
 
     def testPostMoviesFailure(self):
         res = self.client().post('/moviess', json=self.new_movie)
         self.assertEqual(res.status_code, 404)
 
     def testPatchActorSuccess(self):
-        res = self.client().post('/actors/update/1', json=self.new_actor)
+        res = self.client().patch('/actors/update/1', json=self.new_actor,
+                                 headers={"Authorization": "Bearer {}".
+                                          format(self.director_header)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['message'])
+        self.assertTrue(data['message'],'Successfully updated actor')
 
     def testPatchActorFailure(self):
-        res = self.client().post('/actorsss/update/1', json=self.new_actor)
+        res = self.client().patch('/actorsss/update/1', json=self.new_actor)
         self.assertEqual(res.status_code, 404)
 
     def testPatchMovieSuccess(self):
-        res = self.client().post('/movies/update/1', json=self.new_movie)
+        res = self.client().patch('/movies/update/1', json=self.new_movie,
+                                 headers={"Authorization": "Bearer {}".
+                                          format(self.director_header)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['message'])
+        self.assertTrue(data['message'],'Successfully updated movie')
 
     def testPatchMoviesFailure(self):
-        res = self.client().post('/moviesss/update/1', json=self.new_movie)
+        res = self.client().patch('/moviesss/update/1', json=self.new_movie)
         self.assertEqual(res.status_code, 404)
 
     def testDeleteActorSuccess(self):
-        res = self.client().post('/actors/delete/1')
+        res = self.client().delete('/actors/delete/1', headers={"Authorization": "Bearer {}".
+                                format(self.director_header)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['message'])
+        self.assertTrue(data['message'],'Successfully deleted actor')
 
     def testDeleteActorFailure(self):
-        res = self.client().post('/actorsss/delete/1')
+        res = self.client().delete('/actorsss/delete/1')
         self.assertEqual(res.status_code, 404)
 
     def testDeleteMovieSuccess(self):
-        res = self.client().post('/movies/delete/1')
+        res = self.client().delete('/movies/delete/1', headers={"Authorization": "Bearer {}".
+                                format(self.producer_header)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['message'])
+        self.assertTrue(data['message'],'Successfully deleted movie')
 
     def testDeleteMovieFailure(self):
-        res = self.client().post('/moviessss/delete/1')
+        res = self.client().delete('/moviessss/delete/1')
         self.assertEqual(res.status_code, 404)
 
 
